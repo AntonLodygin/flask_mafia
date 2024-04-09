0529b0562304230c0590719cc3a38c5f08d52f14
anton_lodygin <tonylodygin123@yandex.ru>
2024-04-09 20:16:25 +0300

    update README.md

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,6 +1,6 @@
 let xhr = new XMLHttpRequest();
 xhr.open("POST", "http://127.0.0.1:1212/check_lobby_password");
-xhr.send(JSON.stringify({document.getElementById()}))
+xhr.send(JSON.stringify({lobby_id: document.getElementById("hidden_id").val, password: document.getElementById("hidden_id").val}))
 xhr.onload = function() {
   if (xhr.status == 200) { // анализируем HTTP-статус ответа, если статус не 200, то произошла ошибка
     alert(`Ошибка ${xhr.status}: ${xhr.statusText}`); // Например, 404: Not Found
