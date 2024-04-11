abffc392c82e60505c17d42a832957a11e90ceda
anton-baton <tonylodygin123@gmail.com>
2024-04-11 22:36:34 +0300

    password when creating a closed lobby

--- forms/lobby_form.py
+++ forms/lobby_form.py
@@ -7,5 +7,5 @@ class LobbyForm(FlaskForm):
     title = StringField("Название лобби", validators=[DataRequired()], default="lobby")
     users_count = IntegerField("Количество игроков", validators=[DataRequired()], default=10)
     open = BooleanField("Открытое", default=True)
-    password = PasswordField("Пароль", validators=[Length(min=4)])
+    password = PasswordField("Пароль")
     submit = SubmitField("Создать")
